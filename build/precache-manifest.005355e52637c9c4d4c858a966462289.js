84d4c41b6bc2fe29356c0b8a80aff0580c02ce59
vantt <vantt@vnext.com.vn>
2019-09-05 17:16:53 +0700

    build

---
+++ build/precache-manifest.005355e52637c9c4d4c858a966462289.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8b17d7ee91b1e8d06b75add3e4c587c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a772379f7f894076ae90",
+    "url": "/static/css/2.f7e18a81.chunk.css"
+  },
+  {
+    "revision": "a772379f7f894076ae90",
+    "url": "/static/js/2.e6f9ff06.chunk.js"
+  },
+  {
+    "revision": "5a223cb5c01d999f4187",
+    "url": "/static/js/main.020b90ca.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "695a31606b1abb167b03fb1f6375831d",
+    "url": "/static/media/google.695a3160.svg"
+  },
+  {
+    "revision": "a0185b043dee68a72aebffdcc939e21e",
+    "url": "/static/media/logo.a0185b04.svg"
+  }
+]);
